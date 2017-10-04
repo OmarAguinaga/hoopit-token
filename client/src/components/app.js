3d0679d564cfa2ae427b1d82a558daea47e8a6b2
OmarAguinaga <omar.aguinaga94@gmail.com>
2017-10-04 08:51:01 -0500

    Refactor client side for routing

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
 import Header from './header';
+import Home from './home';
+import Signin from './auth/signin';
+import Signout from './auth/signout';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Header />
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/signin" component={Signin} />
+          <Route path="/signout" component={Signout} />
+        </Switch>
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,6 @@ import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import reduxThunk from 'redux-thunk';
 
 import App from './components/app';
-import Signin from './components/auth/signin';
-import Signout from './components/auth/signout';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
@@ -15,11 +13,7 @@ const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router>
-      <div>
-        <Route path="/" component={App} />
-        <Route path="/signin" component={Signin} />
-        <Route path="/signout" component={Signout} />
-      </div>
+      <App />
     </Router>
   </Provider>,
   document.querySelector('.container')
